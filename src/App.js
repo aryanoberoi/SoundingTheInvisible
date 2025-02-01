6070a648975f0dabab8c818141efd1e8705899b4
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2025-02-02 00:14:53 +0530

    created sound button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PollutantPage from "./InfiniteScrollPage";
+import SoundToggle from "./SoundToggle";
 
 const App = () => {
   return (
+    <div>
+      
       <PollutantPage />
-
+      <SoundToggle />
+    </div>
   );
 };
 
