e66b7683ca4728ac4c67057970f4d3aa7962ba61
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2025-02-02 12:51:43 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PollutantPage from "./InfiniteScrollPage";
 import SoundToggle from "./SoundToggle";
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <div>
       
       <PollutantPage />
+      <Navbar />
       <SoundToggle />
     </div>
   );
