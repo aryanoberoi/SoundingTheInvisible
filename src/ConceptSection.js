8792b9a29673dbf3076df6b50f452e000880f16a
aryanoberoi-fanon <1MS20IS025@msrit.edu>
2025-02-20 17:21:02 +0530

    separate components for concept section

--- src/ConceptSection.js
+++ src/ConceptSection.js
@@ -1,16 +1,10 @@
 import React from "react";
 import styles from "./Homepage.css";
 import { ConceptDots } from "./ConceptDots";
-
+import './Conceptsection.css'
 export function ConceptSection() {
   return (
     <section className={styles.conceptSection}>
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/3588bc21f2224826afdf507ac8287e41/74003d4090f30d7918b268263528bb62d3ef772eddacd4be879cec2b69fc899c"
-        className={styles.conceptImage}
-        alt="Concept visual"
-      />
       <div className={styles.conceptContent}>
         <div className={styles.conceptLayout}>
           <article className={styles.conceptArticle}>
@@ -22,14 +16,260 @@ export function ConceptSection() {
               quis, ullamcorper eu neque. Etiam rhoncus erat non quam vehicula.
             </p>
           </article>
+          </div>
+          <div className="frame">
+      <div className="overlap-group">
+        <img
+          className="rectangle"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="img"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-1@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-2"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-2@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-3"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-3@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-4"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-4@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-5"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-5@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-6"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-6@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-7"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-7@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-8"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-8.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-9"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-9@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-10"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-10@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-11"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-11@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-12"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-12.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-13"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-13@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-14"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-14@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-15"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-15@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-16"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-16@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-17"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-17@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-18"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-18@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-19"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-19@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-20"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-20@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-21"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-21@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-22"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-22@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-23"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-23@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-24"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-24@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-25"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-25@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+
+        <img
+          className="rectangle-26"
+          alt="Rectangle"
+          src="https://c.animaapp.com/cVV9xydP/img/rectangle-26@2x.png"
+          style={{ filter: 'brightness(0.4)', transition: 'filter 0.3s' }}
+          onMouseOver={(e) => e.currentTarget.style.filter = 'brightness(1)'}
+          onMouseOut={(e) => e.currentTarget.style.filter = 'brightness(0.4)'}
+        />
+      </div>
           <div className={styles.conceptVisual}>
             <div className={styles.visualContainer}>
-              <img
+              {/* <img
                 loading="lazy"
                 src="https://cdn.builder.io/api/v1/image/assets/3588bc21f2224826afdf507ac8287e41/626056e954b5d7779b52182b95c2f6e0c069bc092a27d67b395539d4710b02db"
                 className={styles.visualImage}
                 alt="Concept illustration"
-              />
+              /> */}
               <ConceptDots />
             </div>
           </div>
