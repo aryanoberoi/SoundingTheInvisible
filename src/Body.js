a994eefcf5c3c0c321e6b2544a6cfac15809f070
arhammxo <anas150052003@gmail.com>
2025-02-22 02:59:03 +0530

    fixed overlap with body

--- src/Body.js
+++ src/Body.js
@@ -3,37 +3,28 @@ import "./Body.css";
 
 export const Box = () => {
   return (
-    <div className="box">
-      <div className="group">
-        <div className="overlap-group-wrapper">
-          <div className="overlap-group">
-            <div className="rectangle" />
-
-            <div className="div" />
-
-            <div className="text-wrapper">Effects on human health</div>
+    <div className="health-effects-box">
+      <div className="content-sections">
+        <div className="header-container">
+          <div className="header-overlap">
+            <div className="header-background" />
+            <div className="header-underline" />
+            <div className="header-title">Effects on human health</div>
           </div>
         </div>
 
-        <div className="overlap-wrapper">
-          <div className="overlap">
+        <div className="human-container">
+          <div className="human-image-wrapper">
             <img
-              className="img"
-              alt="Group"
+              className="main-human-image"
+              alt="Human anatomy diagram"
               src="https://c.animaapp.com/Vg2l9Q1d/img/group-186.png"
             />
-
-            <div className="div-wrapper">
-              <div className="overlap-group-2">
-                {/* <img
-                  className="vector"
-                  alt="Vector"
-                  src="https://c.animaapp.com/Vg2l9Q1d/img/vector-140.svg"
-                /> vector issue */}
-
+            <div className="human-graphics-container">
+              <div className="human-graphics-overlap">
                 <img
-                  className="human"
-                  alt="Human"
+                  className="human-vector"
+                  alt="Anatomy vector"
                   src="https://c.animaapp.com/Vg2l9Q1d/img/human-2.png"
                 />
               </div>
@@ -41,159 +32,20 @@ export const Box = () => {
           </div>
         </div>
 
-        {/* <div className="overlap-2">
-          <img
-            className="image"
-            alt="Image"
-            src="https://c.animaapp.com/Vg2l9Q1d/img/image-5@2x.png"
-          />
-
-          <div className="group-2">
-            <div className="overlap-3">
-              <div className="text-wrapper-2">Place your title here</div>
-
-              <div className="vector-wrapper">
+        {[1, 2, 3, 4, 5, 6, 7].map((item) => (
+          <div className={`content-item content-item-${item}`} key={item}>
+            <div className="item-overlap">
+              <div className="item-title">Place your title here</div>
+              <div className="item-icon-wrapper">
                 <img
-                  className="vector-2"
-                  alt="Vector"
-                  src="https://c.animaapp.com/Vg2l9Q1d/img/vector-148-1.svg"
+                  className="item-icon"
+                  alt="Expand icon"
+                  src={`https://c.animaapp.com/Vg2l9Q1d/img/vector-148-${item % 2 === 0 ? 6 : 4}.svg`}
                 />
               </div>
             </div>
           </div>
-
-          <div className="group-3">
-            <div className="overlap-4">
-              <p className="p">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse congue mollis mauris eget faucibus. Donec fermentum
-                nibh ut gravida imperdiet.Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Suspendisse congue mollis mauris
-                eget faucibus. Donec fermentum nibh ut gravida imperdiet.
-              </p>
-            </div>
-          </div>
-        </div> demo expansion box */}
-
-        {/* <div className="overlap-5">
-          <div className="group-4">
-            <div className="overlap-6">
-              <div className="text-wrapper-3">Place your title here</div>
-
-              <div className="vector-wrapper">
-                <img
-                  className="vector-2"
-                  alt="Vector"
-                  src="https://c.animaapp.com/Vg2l9Q1d/img/vector-148.svg"
-                />
-              </div>
-            </div>
-          </div>
-
-          <img
-            className="image-traced"
-            alt="Image traced"
-            src="https://c.animaapp.com/Vg2l9Q1d/img/image-5--traced-.svg"
-          /> useless cursor
-        </div> */}
-
-        <div className="group-5">
-          <div className="overlap-6">
-            <div className="text-wrapper-3">Place your title here</div>
-
-            <div className="img-wrapper">
-              <img
-                className="vector-3"
-                alt="Vector"
-                src="https://c.animaapp.com/Vg2l9Q1d/img/vector-148-4.svg"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="group-6">
-          <div className="overlap-6">
-            <div className="text-wrapper-3">Place your title here</div>
-
-            <div className="img-wrapper">
-              <img
-                className="vector-3"
-                alt="Vector"
-                src="https://c.animaapp.com/Vg2l9Q1d/img/vector-148-6.svg"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="group-7">
-          <div className="overlap-6">
-            <div className="text-wrapper-3">Place your title here</div>
-
-            <div className="img-wrapper">
-              <img
-                className="vector-3"
-                alt="Vector"
-                src="https://c.animaapp.com/Vg2l9Q1d/img/vector-148-4.svg"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="group-8">
-          <div className="overlap-6">
-            <div className="text-wrapper-3">Place your title here</div>
-
-            <div className="img-wrapper">
-              <img
-                className="vector-3"
-                alt="Vector"
-                src="https://c.animaapp.com/Vg2l9Q1d/img/vector-148-6.svg"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="group-9">
-          <div className="overlap-6">
-            <div className="text-wrapper-3">Place your title here</div>
-
-            <div className="img-wrapper">
-              <img
-                className="vector-3"
-                alt="Vector"
-                src="https://c.animaapp.com/Vg2l9Q1d/img/vector-148-6.svg"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="group-10">
-          <div className="overlap-6">
-            <div className="text-wrapper-3">Place your title here</div>
-
-            <div className="img-wrapper">
-              <img
-                className="vector-3"
-                alt="Vector"
-                src="https://c.animaapp.com/Vg2l9Q1d/img/vector-148-6.svg"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="group-11">
-          <div className="overlap-6">
-            <div className="text-wrapper-3">Place your title here</div>
-
-            <div className="img-wrapper">
-              <img
-                className="vector-3"
-                alt="Vector"
-                src="https://c.animaapp.com/Vg2l9Q1d/img/vector-148-6.svg"
-              />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
