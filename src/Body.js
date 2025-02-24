020c82819419206a0b40006324093127fee34934
arhammxo <anas150052003@gmail.com>
2025-02-24 19:30:15 +0530

    initial body update

--- src/Body.js
+++ src/Body.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./Body.css";
 
 export const Box = () => {
+  const [expandedItem, setExpandedItem] = React.useState(null);
+
+  const toggleItem = (item) => {
+    setExpandedItem(expandedItem === item ? null : item);
+  };
+
   return (
     <div className="health-effects-box">
       <div className="content-sections">
@@ -32,20 +38,32 @@ export const Box = () => {
           </div>
         </div>
 
-        {[1, 2, 3, 4, 5, 6, 7].map((item) => (
-          <div className={`content-item content-item-${item}`} key={item}>
-            <div className="item-overlap">
-              <div className="item-title">Place your title here</div>
-              <div className="item-icon-wrapper">
-                <img
-                  className="item-icon"
-                  alt="Expand icon"
-                  src={`https://c.animaapp.com/Vg2l9Q1d/img/vector-148-${item % 2 === 0 ? 6 : 4}.svg`}
-                />
+        <div>
+          {[1, 2, 3, 4, 5, 6, 7].map((item) => (
+            <div 
+              className={`content-item content-item-${item} ${expandedItem === item ? 'expanded' : ''}`} 
+              key={item}
+              onClick={() => toggleItem(item)}
+            >
+              <div className="item-overlap">
+                <div className="item-title">Place your title here</div>
+                <div className="item-icon-wrapper">
+                  <img
+                    className="item-icon"
+                    alt="Expand icon"
+                    src={`https://c.animaapp.com/Vg2l9Q1d/img/vector-148-${item % 2 === 0 ? 6 : 4}.svg`}
+                  />
+                </div>
+                {expandedItem === item && (
+                  <div className="item-content">
+                    {/* Add your detailed text content here */}
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
+                  </div>
+                )}
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
