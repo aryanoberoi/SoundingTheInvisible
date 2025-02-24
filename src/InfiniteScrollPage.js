bcf8f8456df2e18c8087ce09652a16ba3cef042f
arhammxo <anas150052003@gmail.com>
2025-02-24 13:35:01 +0530

    floating nav test

--- src/InfiniteScrollPage.js
+++ src/InfiniteScrollPage.js
@@ -200,61 +200,54 @@ const PollutantPage = () => {
           </div>
         </div>
       </div>
-
-      <div className="bottom-section1">
-        <div className="main-container">
-          <div className="flex-row-f">
-            <img 
-              src="/ap.png" 
-              alt="Pollutant" 
-              className="image"
-            />
-            <div className="about-pollutant">
-              <span className="about">About </span>
-              <span className="pollutant">Pollutant </span>
+      <div className="combined-section">
+        <div className="bottom-section1">
+          <div className="main-container">
+            <div className="flex-row-f">
+              <img 
+                src="/ap.png" 
+                alt="Pollutant" 
+                className="image"
+              />
+              <div className="about-pollutant">
+                <span className="about">About </span>
+                <span className="pollutant">Pollutant </span>
+              </div>
             </div>
-          </div>
-          <div className="flex-row-a">
+            <div className="flex-row-a">
+              <img 
+                src="l1.png" 
+                alt="Vector graphic" 
+                className="vector-image"
+                style={{ 
+                  width: '100%',
+                  height: 'auto',
+                  objectFit: 'cover' 
+                }}
+              />
+            </div>
+
+            <span className="lorem-ipsum-dolor">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              congue mollis mauris eget faucibus. Donec fermentum nibh ut gravida
+              imperdiet. Donec diam velit, bibendum in volutpat quis, ullamcorper eu
+              neque. Etiam rhoncus erat non quam vehicula, sed maximus magna
+              tincidunt. Sed condimentum sollicitudin nibh, nec mattis quam. Ut eu
+              volutpat nisi, quis varius risus. Integer rutrum eros ac turpis euismod,
+              in tincidunt risus dapibus. Etiam eget turpis massa. Fusce rutrum sit
+              amet magna sit amet aliquam. Donec sit amet cursus erat, sit amet
+              sagittis nunc. Nullam mattis risus nisi, non interdum elit congue in.
+              Donec vitae ligula elit. Morbi nec luctus elit, eu feugiat turpis. Sed
+              porttitor luctus ornare. Suspendisse condimentum fermentum convallis.
+            </span>
             <img 
-              src="l1.png" 
-              alt="Vector graphic" 
-              className="vector-image"
-              style={{ 
-                width: '100%',
-                height: 'auto',
-                objectFit: 'cover' 
-              }}
+              src="g3.png" 
+              alt="graphic element"
+              className="group-3"
             />
           </div>
-
-          <span className="lorem-ipsum-dolor">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            congue mollis mauris eget faucibus. Donec fermentum nibh ut gravida
-            imperdiet. Donec diam velit, bibendum in volutpat quis, ullamcorper eu
-            neque. Etiam rhoncus erat non quam vehicula, sed maximus magna
-            tincidunt. Sed condimentum sollicitudin nibh, nec mattis quam. Ut eu
-            volutpat nisi, quis varius risus. Integer rutrum eros ac turpis euismod,
-            in tincidunt risus dapibus. Etiam eget turpis massa. Fusce rutrum sit
-            amet magna sit amet aliquam. Donec sit amet cursus erat, sit amet
-            sagittis nunc. Nullam mattis risus nisi, non interdum elit congue in.
-            Donec vitae ligula elit. Morbi nec luctus elit, eu feugiat turpis. Sed
-            porttitor luctus ornare. Suspendisse condimentum fermentum convallis.
-          </span>
-          <img 
-            src="g3.png" 
-            alt="graphic element"
-            className="group-3"
-          />
         </div>
-        <div className="nav-bar" style={{ 
-          display: 'flex', 
-          flexDirection: 'column',
-          alignItems: 'center',
-          position: 'relative',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          padding: '20px 0'
-        }}>
+        <div className="nav-bar">
           <div className="text-wrapper">Pollutant name</div>
           <div className="div">Plant name</div>
           <div className="text-wrapper-2">Sound frequency</div>
@@ -284,19 +277,19 @@ const PollutantPage = () => {
             <div className="ellipse-12" />
           </div>
         </div>
-      </div>
 
-      <div className="bottom-section2">
-        <div className="content-container">
-          <h2>Enthalpy and sound Frequency of Pollutant</h2>
+        <div className="bottom-section2">
+          <div className="content-container">
+            <h2>Enthalpy and sound Frequency of Pollutant</h2>
+          </div>
+          <div className="wave-container">
+            <SineWaveVisualizer />
+          </div>
         </div>
-        <div className="wave-container">
-          <SineWaveVisualizer />
-        </div>
-      </div>
 
-      <div className="bottom-section2">
-        <Box></Box>
+        <div className="bottom-section2">
+          <Box></Box>
+        </div>
       </div>
     </>
   );
