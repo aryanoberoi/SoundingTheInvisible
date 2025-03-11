5b520798b9d273be14bc4c6d6333e45c4af34c9b
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2025-03-12 00:23:49 +0530

    page structure

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,75 +1,64 @@
 import React from "react";
 import "./Homepage.css";
-import frame2 from "./frame-2.png";
-import frame3 from "./frame-3.png";
-import frame4 from "./frame-4.png";
-import vector187 from "./vector-187.png";
-import rectangle90 from "./rectangle-90.png"; // Import the rectangle image
+// import frame from "./frame.svg";
+// import frame1 from "./frame.png";
+// import image from "./image.svg";
+// import group283 from "./group-283.png";
+// import vector187 from "./vector-187.svg";
 
-export default function Homepage() {
+export default function Homepage () {
   return (
     <div className="homepage">
-      <div className="container">
 
-        {/* ✅ Box Integration */}
-        <div className="box">
-          <img className="rectangle" alt="Background" src={rectangle90} />
-        </div>
+      {/* 🔸 Header Section */}
+      <section className="header-section">
+        <h1>Sounding The Invisible: An Elegant Symbiosis</h1>
+        <p>Phytoremediation plants from the tropic of the temperate</p>
+      </section>
 
-        {/* Section 1 - Title and Subtitle */}
-        <div className="header-section">
-          <p className="sounding-the">
-            Sounding The Invisible: An Elegant Symbiosis
-          </p>
-          <p className="phytoremediation">
-            Phytoremediation plants from the tropic of the temperate
-          </p>
-        </div>
-
-        {/* Section 2 - Image and Concept */}
-        <div className="concept-section">
-          <img className="frame-2" alt="Concept Art" src={frame2} />
-          <p className="concept-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            congue mollis mauris eget faucibus. Donec fermentum nibh ut gravida
-            imperdiet. Donec diam velit, bibendum in volutpat quis, ullamcorper
-            eu neque. Etiam rhoncus erat non quam vehicula.
-          </p>
-          <div className="section-title">Sound Concept</div>
-        </div>
-
-        {/* Section 3 - Second Image */}
-        <div className="image-section">
-          <img className="frame-3" alt="Concept Extension" src={frame3} />
-        </div>
-
-        {/* Section 4 - Additional Text */}
-        <div className="text-section">
-          <div className="text-content">
-            <img className="frame-4" alt="Additional Concept" src={frame4} />
-            <p className="additional-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse congue mollis mauris eget faucibus. Donec fermentum
-              nibh ut gravida imperdiet. Donec diam velit, bibendum in volutpat
-              quis, ullamcorper eu neque. Etiam rhoncus erat non quam vehicula.
-            </p>
-          </div>
-
-          <div className="concept-title">Concept</div>
-          <p className="concept-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            congue mollis mauris eget faucibus. Donec fermentum nibh ut gravida
-            imperdiet. Donec diam velit, bibendum in volutpat quis, ullamcorper
-            eu neque. Etiam rhoncus erat non quam vehicula.
+      {/* 🔸 Concept Section */}
+      <section className="concept-section">
+        <div className="concept-text">
+          <h2>Concept</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse congue mollis mauris eget faucibus.
           </p>
         </div>
+        {/* <img src={frame} alt="Concept Illustration" className="concept-image" /> */}
+      </section>
 
-        {/* Section 5 - Credits */}
-        <div className="credits-section">
-          <div className="credits-title">Credits</div>
-          <img className="vector-2" alt="Vector Graphic" src={vector187} />
+      {/* 🔸 Black Trapezium Section */}
+      <section className="trapezium-section">
+        {/* <img src={image} alt="Main Illustration" className="trapezium-image" /> */}
+        <div className="trapezium-text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse congue mollis mauris eget faucibus.
+          </p>
         </div>
-      </div>
+      </section>
+
+      {/* 🔸 Sound Strategy Section */}
+      <section className="strategy-section">
+        <div className="strategy-text">
+          <h2>Sound Strategy</h2>
+          <p>
+            Exploring the relationship between phytoremediation plants and their soundscapes within temperate climates.
+          </p>
+        </div>
+        {/* <img src={frame1} alt="Strategy Illustration" className="strategy-image" /> */}
+      </section>
+
+      {/* 🔸 Group Section */}
+      <section className="group-section">
+        {/* <img src={group283} alt="Group Illustration" className="group-image" /> */}
+      </section>
+
+      {/* 🔸 Footer Section */}
+      <footer className="footer-section">
+        <p>Credits</p>
+        {/* <img src={vector187} alt="Vector Design" className="footer-vector" /> */}
+      </footer>
+
     </div>
   );
-}
+};
