24b2f0af15b05fbf6e06636a5f95c06f2ca4e881
arhammxo <anas.21bce7711@vitapstudent.ac.in>
2025-03-22 16:17:19 +0530

    parameterized CaseStudies section

--- src/PollutantPage/CaseStudies.js
+++ src/PollutantPage/CaseStudies.js
@@ -2,89 +2,74 @@ import React from "react";
 import group121 from "./group-121.svg";
 import "./CaseStudies.css";
 
-export const CaseStudies = () => {
-    return (
-      <div className="box">
-        <div className="group">
-          <div className="overlap">
-            <div className="overlap-group">
-              <p className="text-wrapper">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse congue mollis mauris eget faucibus. Donec fermentum
-                nibh ut gravida imperdiet. Donec diam velit, bibendum in volutpat
-                quis, ullamcorper eu neque. Etiam rhoncus erat non quam vehicula,
-                sed maximus magna tincidunt. Sed condimentum sollicitudin nibh,
-                nec mattis quam. Ut eu volutpat nisi, quis varius risus. Integer
-                rutrum eros ac turpis euismod, in tincidunt risus dapibus. Etiam
-                eget turpis massa. Fusce rutrum sit amet magna sit amet aliquam.
-                Donec sit amet cursus erat, sit amet sagittis nunc. Nullam mattis
-                risus nisi, non interdum elit congue in. Donec vitae ligula elit.
-                Morbi nec luctus elit, eu feugiat turpis. Sed porttitor luctus
-                ornare. Suspendisse condimentum fermentum convallis.
-              </p>
-  
-              <img
-                className="rectangle"
-                alt="Rectangle"
-                src="https://c.animaapp.com/2uQFoxD8/img/rectangle-28.svg"
-              />
-  
-              <p className="div">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse congue mollis mauris eget faucibus. Donec fermentum
-                nibh ut gravida imperdiet. Donec diam velit, bibendum in volutpat
-                quis, ullamcorper eu neque. Etiam rhoncus erat non quam vehicula,
-                sed maximus magna tincidunt. Sed condimentum sollicitudin nibh,
-                nec mattis quam. Ut eu volutpat nisi, quis varius risus. Integer
-                rutrum eros ac turpis euismod, in tincidunt risus dapibus. Etiam
-                eget turpis massa. Fusce rutrum sit amet magna sit amet aliquam.
-                Donec sit amet cursus erat, sit amet sagittis nunc. Nullam mattis
-                risus nisi, non interdum elit congue in. Donec vitae ligula elit.
-                Morbi nec luctus elit, eu feugiat turpis. Sed porttitor luctus
-                ornare. Suspendisse condimentum fermentum convallis.
-              </p>
-  
-              <div className="overlap-wrapper">
-                <div className="overlap-2">
-                  <div className="overlap-3">
-                    <img
-                      className="group-121"
-                      alt="Group"
-                      src={group121}
-                    />
-                  </div>  
-                </div>
-              </div>
-  
-              <div className="text-wrapper-2">Area</div>
-  
-              <img
-                className="rectangle-5"
-                alt="Rectangle"
-                src="https://c.animaapp.com/2uQFoxD8/img/rectangle-28-1.svg"
-              />
-            </div>
-  
+export const CaseStudies = ({ sections }) => {
+  // Helper function to get title and content from text
+  const parseSection = (text) => {
+    const [title, content] = text.split('_');
+    return { title, content };
+  };
+
+  const name = parseSection(sections[0].text);
+  const location = parseSection(sections[1].text);
+
+  return (
+    <div className="box">
+      <div className="group">
+        <div className="overlap">
+          <div className="overlap-group">
+            <p className="text-wrapper">
+              {location.content}
+            </p>
+
             <img
-              className="vector-3"
-              alt="Vector"
-              src="https://c.animaapp.com/2uQFoxD8/img/vector-109.svg"
+              className="rectangle"
+              alt="Rectangle"
+              src="https://c.animaapp.com/2uQFoxD8/img/rectangle-28.svg"
+            />
+
+            <p className="div">
+              {name.content}
+            </p>
+
+            <div className="overlap-wrapper">
+              <div className="overlap-2">
+                <div className="overlap-3">
+                  <img
+                    className="group-121"
+                    alt="Group"
+                    src={group121}
+                  />
+                </div>  
+              </div>
+            </div>
+
+            <div className="text-wrapper-2">{location.title}</div>
+
+            <img
+              className="rectangle-5"
+              alt="Rectangle"
+              src="https://c.animaapp.com/2uQFoxD8/img/rectangle-28-1.svg"
             />
           </div>
-  
-          <div className="overlap-group-wrapper">
-            <div className="overlap-group-2">
-              <div className="rectangle-6" />
-  
-              <div className="rectangle-7" />
-  
-              <div className="text-wrapper-3">Case studies</div>
-            </div>
-          </div>
-  
-          <div className="text-wrapper-4">Venice lagoon</div>
+
+          <img
+            className="vector-3"
+            alt="Vector"
+            src="https://c.animaapp.com/2uQFoxD8/img/vector-109.svg"
+          />
         </div>
+
+        <div className="overlap-group-wrapper">
+          <div className="overlap-group-2">
+            <div className="rectangle-6" />
+            <div className="rectangle-7" />
+            <div className="text-wrapper-3">Case studies</div>
+          </div>
+        </div>
+
+        <div className="text-wrapper-4">{name.title}</div>
       </div>
-    );
-  };
+    </div>
+  );
+};
   
