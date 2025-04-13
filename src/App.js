108edc838be9fa34fb09efc3282fcc0f7f83962a
aryanoberoi-fanon <1MS20IS025@msrit.edu>
2025-04-13 23:48:35 +0530

    changed cursor

--- src/App.js
+++ src/App.js
@@ -69,15 +69,13 @@ const AppContent = () => {
 
   return (
     <div>
-    <div style={{ maxHeight: '100vh', overflow: 'hidden' }}>
       <Navbar />
-      </div>
       <SoundToggle />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/:customName" element={<PollutantPage categorizedData={categorizedData} />} />
       </Routes>
-      </div>
+    </div>
   );
 };
 
