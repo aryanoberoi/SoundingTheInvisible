57ca3e5470110781ebc85f1fdd09287e48a174f6
arhammxo <anas.21bce7711@vitapstudent.ac.in>
2025-05-06 22:50:28 +0530

    updated arrow

--- src/PollutantPage/LeftPanel.js
+++ src/PollutantPage/LeftPanel.js
@@ -76,8 +76,8 @@ const LeftPanel = ({ sections = [], onLoad, onNavigate }) => {
               {/* Animated Down Arrow */}
       <div className="animated-down-arrow">
         <svg width="42" height="52" viewBox="0 0 42 52" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M21 3V37" stroke="white" strokeWidth="2" strokeLinecap="round"/>
-          <path d="M16 31L21 40L26 31" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M21 3V37" stroke="white" strokeWidth="1.5" strokeLinecap="round"/>
+          <path d="M18 31L21 41L24 31" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
       </div>
     </div>
